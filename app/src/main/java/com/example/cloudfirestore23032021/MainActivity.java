3cadaed93cc65ec03acef259c9b16ad6242ca907
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-13 21:24:06 +0700

    read data document real time

--- app/src/main/java/com/example/cloudfirestore23032021/MainActivity.java
+++ app/src/main/java/com/example/cloudfirestore23032021/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cloudfirestore23032021;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,22 +36,22 @@ public class MainActivity extends AppCompatActivity {
 //                    - ten : Lap trinh android
 //            - ios
 //                    - ten : Lap trinh ios
-        Map<String,String> mapCourse = new HashMap<>();
-        mapCourse.put("ten", "Lập trình android");
-
-        db.collection("khoahoc")
-                .document("android")
-                .set(mapCourse)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+//        Map<String,String> mapCourse = new HashMap<>();
+//        mapCourse.put("ten", "Lập trình ios");
+//
+//        db.collection("khoahoc")
+//                .document("ios")
+//                .set(mapCourse)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()){
+//                            Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+//                        }else{
+//                            Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
         // 2 Gui dang mang
 //        Map<String , List<String>> mapNames = new HashMap<>();
 //        mapNames.put("arrays", new ArrayList<>(Arrays.asList("Tèo" , "Tí" , "Tủn" , "Hoa")));
@@ -65,5 +69,46 @@ public class MainActivity extends AppCompatActivity {
 //                        }
 //                    }
 //                });
+
+        //3 : Gui dang object
+//        Animal animal = new Animal("Dog",5);
+//
+//        db.collection("animal")
+//                .document("athome")
+//                .set(animal)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()){
+//                            Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+//                        }else{
+//                            Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+
+        // 4 : Gui dang random id
+//        Product product = new Product("product2",20000);
+//        db.collection("product")
+//                .add(product);
+
+        /* ================================================================ */
+
+        // 1  Doc du lieu dang string
+        DocumentReference khoahocRef =  db.collection("khoahoc").document("android");
+        khoahocRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                String chuoi = "";
+                if (value != null && value.exists()) {
+                    chuoi = (String) value.getData().get("ten");
+                    Toast.makeText(MainActivity.this, chuoi, Toast.LENGTH_SHORT).show();
+                } else {
+                    System.out.print("Current data: null");
+                }
+
+            }
+        });
+
     }
 }
